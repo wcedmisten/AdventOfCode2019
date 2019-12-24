864bfe16bcd0f4caa29fda98a1d81597a57fa130
William Edmisten <wce@vt.edu>
2019-12-23 22:41:42 -0500

    Added small test cases to unit test

--- day2/test_dumb_computer.py
+++ day2/test_dumb_computer.py
@@ -11,13 +11,44 @@ class Tests(unittest.TestCase):
     """
     def test_big_example(self):
         """Tests the DumbComputer.process_instructions function with the
-        big provided example
+        big provided example.
 
         """
         computer = DumbComputer("1,9,10,3,2,3,11,0,99,30,40,50")
         self.assertEqual(computer.process_instructions(),
                             "3500,9,10,70,2,3,11,0,99,30,40,50")
 
+    def test_first_small_example(self):
+        """Tests the DumbComputer.process_instructions function with the
+        first small example.
+
+        """
+        computer = DumbComputer("1,0,0,0,99")
+        self.assertEqual(computer.process_instructions(), "2,0,0,0,99")
+
+    def test_second_small_example(self):
+        """Tests the DumbComputer.process_instructions function with the
+        second small example.
+
+        """
+        computer = DumbComputer("2,3,0,3,99")
+        self.assertEqual(computer.process_instructions(), "2,3,0,6,99")
+
+    def test_third_small_example(self):
+        """Tests the DumbComputer.process_instructions function with the
+        third small example.
+
+        """
+        computer = DumbComputer("2,4,4,5,99,0")
+        self.assertEqual(computer.process_instructions(), "2,4,4,5,99,9801")
+
+    def test_fourth_small_example(self):
+        """Tests the DumbComputer.process_instructions function with the
+        fourth small example.
+
+        """
+        computer = DumbComputer("1,1,1,4,99,5,6,0,99")
+        self.assertEqual(computer.process_instructions(), "30,1,1,4,2,5,6,0,99")
 
 
 if __name__ == '__main__':
